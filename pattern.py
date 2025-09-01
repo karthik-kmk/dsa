f4d80165b701c6957aa0fa615af360e0b96198d6
Acers <karthik.kmk.krishna@gmail.com>
2025-09-01 10:20:58 +0530

    tasks,bit,pattern

---
+++ pattern.py
@@ -0,0 +1,17 @@
+# square
+# def square(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print("*",end="")
+#         print()
+# square(5)
+
+# right angled triangle
+
+# def right(n):
+#     for i in range(1, n+1):        
+#         for j in range(i):         
+#             print("*", end="")
+#         print()                    
+
+# right(5)
---
+++ tasks.py
@@ -0,0 +1,53 @@
+# print 1 to 100
+
+# def printn(n):
+#     for i in range(1,n+1):
+#         return i
+# printn(100)        
+
+# def even(n):
+#     even = []
+#     for i in range(1,n+1):
+#         if i % 2 == 0:
+#             even.append(i)
+#     return even
+        
+# print(even(100))
+
+
+
+# def odd(n):
+#     odd = []
+#     for i in range(1,n+1):
+#         if i % 2 != 0:
+#             odd.append(i)
+#     return odd
+# print(odd(100))
+
+
+# # number divisble by 3 and 5
+
+# def divisible(n):
+#     divisible = []
+#     for i in range(1,n+1):
+#         if i % 3 == 0 and i % 5 == 0:
+#             divisible.append(i)
+#     return divisible
+# print(divisible(20))
+            
+# sum of first n natural numbers
+# def natural(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i
+#     return sum
+# print(natural(10))
+
+
+# hcf
+
+# def hcf(a,b):
+#     while b!=0:
+#         a,b = b,a%b
+#     return a
+# print(hcf(48,18))
